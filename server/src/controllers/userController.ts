805a777b896ed98fa5fc218c053b91b38f01eb60
Reimar Rosas <reimarrosas0208@gmail.com>
2021-11-07 21:32:07 -0500

    Added a create user method in the user controller that returns a Promise that resolves to error if insertion failed or null if insertion successful

---
+++ server/src/controllers/userController.ts
@@ -0,0 +1,16 @@
+import db from '../database/init';
+import HttpError from "../utils/httpError";
+import { IUser, Maybe } from "../utils/types";
+
+export async function createUser(user: IUser): Promise<Maybe<HttpError>> {
+  try {
+    await db.none(`
+      INSERT INTO USERS (NAME, EMAIL, PASSWORD)
+      VALUES ($<name>, $<email>, $<password>);
+    `, user);
+  } catch(err: any) {
+    return new HttpError(500, 'API/Database Error', err.message, err.stack);
+  }
+
+  return Promise.resolve(null);
+};
