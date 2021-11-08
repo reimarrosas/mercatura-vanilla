32207191e700144447f22cad3f58b97f642b905f
Reimar Rosas <reimarrosas0208@gmail.com>
2021-11-08 14:40:30 -0500

    Changed the return type from promise IUser to promise number

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,26 +1,46 @@
 import db from '../database/init';
 import HttpError from '../utils/httpError';
-import { IUser, Maybe } from "../utils/types";
+import isNothing from '../utils/isNothing';
+import { IUser } from "../utils/types";
 
-export async function searchUserByEmail(user: IUser): Promise<Maybe<IUser>> {
+export async function searchUserByEmail(user: IUser): Promise<number> {
+  let id;
   try {
-    const id = await db.oneOrNone<IUser>(`
+    id = await db.oneOrNone(`
       SELECT USER_ID FROM USERS WHERE USER_EMAIL = $<email>
     `, { email: user.email });
-
-    return id;
-  } catch(err: any) {
+  } catch (err: any) {
     throw new HttpError(500, 'API/Database Error', err.message, err.stack);
   }
+  
+  return id;
 }
+
+
 
 export async function createUser(user: IUser): Promise<void> {
   try {
+    const id =
+      await db.oneOrNone(`
+        SELECT USER_ID FROM USERS WHERE USER_EMAIL = $<email>
+      `, { email: user.email });
+
+    if (!isNothing(id)) {
+      throw new HttpError(
+        409,
+        'User Already Exists',
+        `The desired user email, ${user.email}, already exists.`
+      );
+    }
+    
     await db.none(`
       INSERT INTO USERS (USER_NAME, USER_EMAIL, USER_PASSWORD)
       VALUES ($<name>, $<email>, $<password>);
     `, user);
   } catch(err: any) {
+    if (!isNothing(err.statusCode)) {
+      throw err;
+    }
     throw new HttpError(500, 'API/Database Error', err.message, err.stack);
   }
 };
