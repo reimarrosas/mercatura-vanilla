ea31d73319ac053e123b60f2b869a86c9dcc9a50
Reimar Rosas <reimarrosas0208@gmail.com>
2021-11-07 22:38:19 -0500

    Implemented the login route

---
+++ server/src/routes/userRoutes.ts
@@ -0,0 +1,22 @@
+import express, { NextFunction, Request, Response } from 'express';
+
+import { createUser } from '../controllers/userController';
+import hashPassword from '../services/hashPassword';
+import validateUserCreds from '../services/validateUserCreds';
+import errorThrower from '../utils/errorThrower';
+import { IUser } from '../utils/types';
+
+const router = express.Router();
+
+router.post('login', async (req: Request, res: Response, next: NextFunction) => {
+  const credentials: IUser = req.body;
+  try {
+    errorThrower(validateUserCreds(credentials));
+
+    const hashedUser: IUser = await hashPassword(credentials);
+
+    errorThrower(await createUser(hashedUser));
+  } catch (err) {
+    return next(err);
+  }
+});
