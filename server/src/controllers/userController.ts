8cf12aa098084651811fab49ef8fa061994b4d39
Reimar Rosas <reimarrosas0208@gmail.com>
2021-11-08 14:47:09 -0500

    Added searchUserByCredential for fetching user for auth

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,7 +1,7 @@
 import db from '../database/init';
 import HttpError from '../utils/httpError';
 import isNothing from '../utils/isNothing';
-import { IUser } from "../utils/types";
+import { IUser, Maybe } from "../utils/types";
 
 export async function searchUserByEmail(user: IUser): Promise<number> {
   let id;
@@ -12,11 +12,25 @@ export async function searchUserByEmail(user: IUser): Promise<number> {
   } catch (err: any) {
     throw new HttpError(500, 'API/Database Error', err.message, err.stack);
   }
-  
+
   return id;
 }
 
+export async function searchUserByCredentials(user: IUser): Promise<Maybe<IUser>> {
+  let queriedUser: Maybe<IUser>;
 
+  try {
+    queriedUser = await db.oneOrNone<Maybe<IUser>>(`
+      SELECT USER_ID, USER_EMAIL, USER_PASSWORD
+      FROM USERS
+      WHERE USER_EMAIL = $<email>;
+    `, { email: user.email });
+  } catch (err: any) {
+    throw new HttpError(500, 'API/Database Error', err.message, err.stack);
+  }
+
+  return queriedUser;
+}
 
 export async function createUser(user: IUser): Promise<void> {
   try {
