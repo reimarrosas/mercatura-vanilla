de3854fcc57ff2b42f2b8acf4ee86aa75389944f
Reimar Rosas <reimarrosas0208@gmail.com>
2021-11-08 13:46:39 -0500

    Removed the errorThrower references

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -3,8 +3,6 @@ import express, { NextFunction, Request, Response } from 'express';
 import { createUser } from '../controllers/userController';
 import hashPassword from '../services/hashPassword';
 import validateUserCreds from '../services/validateUserCreds';
-import errorThrower from '../utils/errorThrower';
-import HttpError from '../utils/httpError';
 import { IUser } from '../utils/types';
 
 const router = express.Router();
@@ -13,12 +11,10 @@ router.post('/signup', async (req: Request, res: Response, next: NextFunction) =
   const credentials: IUser = req.body;
 
   try {
-    errorThrower(await validateUserCreds(credentials));
+    await validateUserCreds(credentials)
     const hashedUser: IUser = await hashPassword(credentials);
-
-    errorThrower(await createUser(hashedUser));
-  } catch (err) {
-    console.log(`It's HERE!! ${err}`);
+    await createUser(hashedUser);
+  } catch (err: any) {
     return next(err);
   }
 
