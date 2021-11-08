8311d935ff31bec635989fd8935e4e54d44975dc
Reimar Rosas <reimarrosas0208@gmail.com>
2021-11-08 13:45:07 -0500

    Throws error instead of letting the errorThrower handle it

--- server/src/services/validateUserCreds.ts
+++ server/src/services/validateUserCreds.ts
@@ -2,12 +2,10 @@ import HttpError from "../utils/httpError";
 import { IHttpError, IUser, Maybe } from "../utils/types";
 import userValidator from "./schemaValidators/userValidator";
 
-export default async (user: IUser): Promise<Maybe<IHttpError>> => {
+export default async (user: IUser): Promise<void> => {
   try {
-    const value = await userValidator.validateAsync(user);
+    await userValidator.validateAsync(user);
   } catch (err: any) {
-    return new HttpError(400, 'User Validation Error', err.message, err.stack);
+    throw new HttpError(400, 'User Validation Error', err.message, err.stack);
   }
-
-  return Promise.resolve(null);
 }
