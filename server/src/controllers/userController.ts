77948e165bf27887edfa8ca76b4454c968ce78af
Reimar Rosas <reimarrosas0208@gmail.com>
2021-11-08 13:46:17 -0500

    Throws an error instead of letting the errorThrower throws it, and checks if the user entry already exists within the database before inserting

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,16 +1,31 @@
 import db from '../database/init';
 import HttpError from '../utils/httpError';
-import { IHttpError, IUser, Maybe } from "../utils/types";
+import isNothing from '../utils/isNothing';
+import { IUser } from "../utils/types";
 
-export async function createUser(user: IUser): Promise<Maybe<IHttpError>> {
+export async function createUser(user: IUser): Promise<void> {
   try {
+    const id =
+      await db.oneOrNone(`
+        SELECT USER_ID FROM USERS WHERE USER_EMAIL = $<email>
+      `, { email: user.email });
+
+    if (!isNothing(id)) {
+      throw new HttpError(
+        409,
+        'User Already Exists',
+        `The desired user email, ${user.email}, already exists.`
+      );
+    }
+    
     await db.none(`
       INSERT INTO USERS (USER_NAME, USER_EMAIL, USER_PASSWORD)
       VALUES ($<name>, $<email>, $<password>);
     `, user);
   } catch(err: any) {
-    return new HttpError(500, 'API/Database Error', err.message, err.stack);
+    if (!isNothing(err.statusCode)) {
+      throw err;
+    }
+    throw new HttpError(500, 'API/Database Error', err.message, err.stack);
   }
-
-  return Promise.resolve(null);
 };
