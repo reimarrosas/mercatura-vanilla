d7a9c349d7d505d3fdb26ecd1ea9b8130e183b8a
Reimar Rosas <reimarrosas0208@gmail.com>
2021-11-08 00:31:11 -0500

    Added the body parser and the routes/handlers

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,11 +5,14 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import userRoutes from './routes/userRoutes';
+import notFoundHandler from './handlers/notFoundHandler';
+import errorHandler from './handlers/errorHandler';
 
 const app = express();
 
 app.use(cors());
 app.use(helmet());
+app.use(express.json());
 
 const PORT = process.env['PORT'] ?? 5131;
 
@@ -24,3 +27,5 @@ app.listen(PORT, () => {
 });
 
 app.use('/api/users', userRoutes);
+app.use(notFoundHandler);
+app.use(errorHandler);
