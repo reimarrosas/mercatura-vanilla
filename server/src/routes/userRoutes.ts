66986e306bb0659b4160d38b9177930345c82d4e
Reimar Rosas <reimarrosas0208@gmail.com>
2021-11-08 00:33:07 -0500

    Implemented the signup route, still debugging the weird 404 error from the validateUser service

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -4,21 +4,28 @@ import { createUser } from '../controllers/userController';
 import hashPassword from '../services/hashPassword';
 import validateUserCreds from '../services/validateUserCreds';
 import errorThrower from '../utils/errorThrower';
+import HttpError from '../utils/httpError';
 import { IUser } from '../utils/types';
 
 const router = express.Router();
 
-router.post('signup', async (req: Request, res: Response, next: NextFunction) => {
+router.post('/signup', async (req: Request, res: Response, next: NextFunction) => {
   const credentials: IUser = req.body;
-  try {
-    errorThrower(validateUserCreds(credentials));
 
+  try {
+    errorThrower(await validateUserCreds(credentials));
     const hashedUser: IUser = await hashPassword(credentials);
 
     errorThrower(await createUser(hashedUser));
   } catch (err) {
+    console.log(`It's HERE!! ${err}`);
     return next(err);
   }
+
+  res.status(201)
+      .send({
+        message: 'User created.'
+      });
 });
 
 export default router;
