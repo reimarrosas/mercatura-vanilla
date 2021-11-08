6ba2738be2857bc5ed7faa0a5053394293ec9940
Reimar Rosas <reimarrosas0208@gmail.com>
2021-11-08 13:45:32 -0500

    Make the properties required

--- server/src/services/schemaValidators/userValidator.ts
+++ server/src/services/schemaValidators/userValidator.ts
@@ -3,11 +3,14 @@ import joi from 'joi';
 export default joi.object({
   name: joi.string()
             .trim()
-            .min(1),
+            .min(1)
+            .required(),
   email: joi.string()
             .trim()
-            .email(),
+            .email()
+            .required(),
   password: joi.string()
                 .trim()
                 .min(6)
+                .required()
 });
