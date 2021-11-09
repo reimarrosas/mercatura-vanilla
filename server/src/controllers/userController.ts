a5ad772f692012ba91a71ece5220ac1c346d3c62
Reimar Rosas <reimarrosas0208@gmail.com>
2021-11-09 14:27:02 -0500

    Made the searchUserByEmail function to return all the necessary data instead of just the id, and removed the existence check in the createUser to conform to SRP

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -3,28 +3,12 @@ import HttpError from '../utils/httpError';
 import isNothing from '../utils/isNothing';
 import { IUser, Maybe } from "../utils/types";
 
-export async function searchUserByEmail(user: IUser): Promise<number> {
-  let id;
-  try {
-    id = await db.oneOrNone(`
-      SELECT USER_ID FROM USERS WHERE USER_EMAIL = $<email>
-    `, { email: user.email });
-  } catch (err: any) {
-    throw new HttpError(500, 'API/Database Error', err.message, err.stack);
-  }
-
-  return id;
-}
-
-export async function searchUserByCredentials(user: IUser): Promise<Maybe<IUser>> {
+export async function searchUserByEmail(user: IUser): Promise<Maybe<IUser>> {
   let queriedUser: Maybe<IUser>;
-
   try {
-    queriedUser = await db.oneOrNone<Maybe<IUser>>(`
-      SELECT USER_ID, USER_EMAIL, USER_PASSWORD
-      FROM USERS
-      WHERE USER_EMAIL = $<email>;
-    `, { email: user.email });
+    queriedUser = await db.oneOrNone(`
+      SELECT USER_ID, USER_NAME, USER_EMAIL, USER_PASSWORD FROM USERS WHERE USER_EMAIL = $<email>
+    `, { email: user.user_email });
   } catch (err: any) {
     throw new HttpError(500, 'API/Database Error', err.message, err.stack);
   }
@@ -34,27 +18,11 @@ export async function searchUserByCredentials(user: IUser): Promise<Maybe<IUser>
 
 export async function createUser(user: IUser): Promise<void> {
   try {
-    const id =
-      await db.oneOrNone(`
-        SELECT USER_ID FROM USERS WHERE USER_EMAIL = $<email>
-      `, { email: user.email });
-
-    if (!isNothing(id)) {
-      throw new HttpError(
-        409,
-        'User Already Exists',
-        `The desired user email, ${user.email}, already exists.`
-      );
-    }
-    
     await db.none(`
       INSERT INTO USERS (USER_NAME, USER_EMAIL, USER_PASSWORD)
       VALUES ($<name>, $<email>, $<password>);
     `, user);
   } catch(err: any) {
-    if (!isNothing(err.statusCode)) {
-      throw err;
-    }
     throw new HttpError(500, 'API/Database Error', err.message, err.stack);
   }
 };
