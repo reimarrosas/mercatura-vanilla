3794fa03d12c6c32df57213673e515c0e504fc19
Reimar Rosas <reimarrosas0208@gmail.com>
2021-11-09 14:29:45 -0500

    Renamed the user object properties to conform to the new IUser interface

--- server/src/services/hashPassword.ts
+++ server/src/services/hashPassword.ts
@@ -3,15 +3,15 @@ import argon2 from "argon2";
 import HttpError from "../utils/httpError";
 
 export default async (user: IUser): Promise<IUser> => {
-  const hash = await argon2.hash(user.password, {
+  const hash = await argon2.hash(user.user_password, {
     type: argon2.argon2id,
     memoryCost: 2 ** 14,
     hashLength: 50,
   });
 
   return {
-    name: user.name,
-    email: user.email,
-    password: hash,
+    user_name: user.user_name,
+    user_email: user.user_email,
+    user_password: hash,
   };
 };
