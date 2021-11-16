af48366fe3fadb22192ee9b433cb00755f90db49
Reimar Rosas <reimarrosas0208@gmail.com>
2021-11-16 13:46:35 -0500

    Implemented the fetching of category and inserting it in the dom as cards

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,13 +1,31 @@
 "use strict";
 
-(function () {
-  const cardList = document.querySelector('.categories__card-list');
-  console.log(cardList);
-})();
+const fetchCategories = async (relativeUrl) => {
+  const fullUrl = `${window.sessionStorage.getItem('baseApiEndpoint')}/${relativeUrl}`
+  const response = await fetch(fullUrl);
 
-const fetchCategories = () => {
-
+  return response;
 };
+
+window.onload = async function() {
+  const cardList = document.querySelector('.categories__card-list');
+  const response = await fetchCategories('api/categories')
+  const responseArr = await response.json();
+  const fst4Categories = [...await responseArr.slice(0, 4)];
+  populateCategoryList(fst4Categories, cardList);
+};
+
+const populateCategoryList = (categories, categoryList) => {
+  categories.forEach(category => {
+    createCard(category.category_image, category.category_name, categoryList);
+  })
+  const viewMoreCard = document.createElement('li');
+  viewMoreCard.classList.add('categories__card');
+  viewMoreCard.innerHTML = `
+    <a href="pages/categories.html" class="view__more">View More!</a>
+  `;
+  categoryList.appendChild(viewMoreCard);
+}
 
 const createCard = (imgSrc, categoryName, listElement) => {
   const listItem = document.createElement('li');
