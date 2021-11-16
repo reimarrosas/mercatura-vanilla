5c143daa2b86649288ca9e2b0ea92dcd0e545b4d
Reimar Rosas <reimarrosas0208@gmail.com>
2021-11-16 13:25:34 -0500

    Implemented the category card creation

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,1 +1,23 @@
 "use strict";
+
+(function () {
+  const cardList = document.querySelector('.categories__card-list');
+  console.log(cardList);
+})();
+
+const fetchCategories = () => {
+
+};
+
+const createCard = (imgSrc, categoryName, listElement) => {
+  const listItem = document.createElement('li');
+  listItem.classList.add('categories__card');
+  listItem.innerHTML = `
+    <div class="container__img">
+      <img src="${imgSrc}" alt="${categoryName}." />
+    </div>
+    <a href="" class="category__link">${categoryName}</a>
+  `;
+
+  listElement.appendChild(listItem);
+};
