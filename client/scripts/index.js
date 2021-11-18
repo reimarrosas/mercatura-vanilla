2ec955e9eb4aee8ab2d6df08a41b3e241bc4c7da
Reimar Rosas <reimarrosas0208@gmail.com>
2021-11-18 10:17:08 -0500

    Fixed responsiveness and links in the index page

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -27,30 +27,32 @@ const populateCategoryList = (categories, categoryList) => {
   categories.forEach(category => {
     createCard(category, categoryList);
   })
-  const viewMoreCard = document.createElement('li');
-  viewMoreCard.classList.add('categories__card', 'flex-center', 'view-more');
-  viewMoreCard.innerHTML = `
-    <a href="pages/categories.html" class="view__more">View More!<span class="arrow">></span></a>
-  `;
-  categoryList.appendChild(viewMoreCard);
+  const anchor = document.createElement('a');
+  anchor.classList.add('categories__card');
+  anchor.href = '/pages/categories.html';
+  const listItem = document.createElement('li');
+  listItem.classList.add('view-more', 'flex-center');
+  listItem.innerHTML = 'View More!<span>></span>'
+  anchor.appendChild(listItem);
+  categoryList.appendChild(anchor);
 }
 
 const createCard = (category, listElement) => {
+  const anchor = document.createElement('a');
+  anchor.classList.add('categories__card');
   const listItem = document.createElement('li');
-  listItem.classList.add('categories__card', 'flex-center');
+  listItem.classList.add('categories__list-item', 'flex-center');
   listItem.innerHTML = `
     <div class="container__img">
       <img src="${category.category_image}" alt="${category.category_name}." />
     </div>
+    <span>${category.category_name}</span>
   `;
-  const anchor = document.createElement('a');
-  anchor.classList.add('category__link');
-  anchor.textContent = category.category_name;
   anchor.addEventListener('click', () => {
     window.sessionStorage.setItem('selectedCategory', JSON.stringify(category));
     window.location.href = '/pages/query.html';
   });
-  listItem.appendChild(anchor);
+  anchor.appendChild(listItem);
 
-  listElement.appendChild(listItem);
+  listElement.appendChild(anchor);
 };
